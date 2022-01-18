d277b37c08b825dfbac9de21df6935eedc2a9035
mimansha-swarup <mimanshaswarup@gmail.com>
2022-01-18 14:34:33 +0530

    assets

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import kantoMap from "./gen/kanto.jpg"
+import jhotoMap from "./gen/jhoto.jpg"
+import hoennMap from "./gen/hoenn.jpg"
+import sinnohMap from "./gen/sinnoh.jpg"
+import unnovaMap from "./gen/unnova.jpg"
+import kalosMap from "./gen/kalos.jpg"
+import alolaMap from "./gen/alola.jpg"
+import galarMap from "./gen/galar.jpg"
+
+export { kantoMap,jhotoMap,hoennMap,sinnohMap,unnovaMap,galarMap,kalosMap,alolaMap}
